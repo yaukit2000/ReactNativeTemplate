a20009c8f0d4820c5de6566a3edebd5521c53eee
York <nahckroy@gmail.com>
2021-01-02 10:13:53 +0800

    remove code

--- src/redux/sagas/auth.saga.ts
+++ src/redux/sagas/auth.saga.ts
@@ -13,26 +13,11 @@ export const getAuth = (state: TAppState) => state.auth;
 
 
 function* login(action: Login) {
-    // yield secure(function* () {
-    //     yield logout();
-    //     const { username, password } = action;
-    //     const { sessionToken, userId, roleName } = yield call(async () => await apiClient.signIn(username, password));
-    //     yield put({ type: LOGIN_DONE, sessionToken, userId, roleName, username });
-    //     if (roleName === 'staff' || roleName === 'admin') {
-    //         globalVar.history.push('/event')
-    //     }
-    // }, true);
+    
 }
 
 function* logout(action?: any) {
-    // try {
-    //     yield fork(async () => apiClient.signOut());
-    // } catch (error) {
-    //     console.error(error);
-    // } finally {
-    //     yield put({ type: LOGOUT_DONE });
-    //     globalVar.history.push('/login')
-    // }
+    
 }
 
 function* watchLogin() {
